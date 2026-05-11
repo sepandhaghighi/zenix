de02c3d393958f1d0897b759ebd22e4a3bf2cc4d
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-05-11 17:55:01 +0330

    fix : merge sample-rate errors

--- zenix/functions.py
+++ zenix/functions.py
@@ -12,7 +12,7 @@ from .params import DEFAULT_VOLUME, DEFAULT_FADE_IN, DEFAULT_FADE_OUT
 from .params import NoiseType
 from .params import INVALID_NOISE_TYPE_ERROR
 from .params import INVALID_DURATION_ERROR
-from .params import INVALID_SAMPLE_RATE_TYPE_ERROR, INVALID_SAMPLE_RATE_VALUE_ERROR
+from .params import INVALID_SAMPLE_RATE_ERROR
 from .params import INVALID_VOLUME_TYPE_ERROR, INVALID_VOLUME_RANGE_ERROR
 from .params import INVALID_FADE_IN_TYPE_ERROR, INVALID_FADE_IN_VALUE_ERROR, INVALID_FADE_IN_RANGE_ERROR
 from .params import INVALID_FADE_OUT_TYPE_ERROR, INVALID_FADE_OUT_VALUE_ERROR, INVALID_FADE_OUT_RANGE_ERROR
@@ -44,10 +44,10 @@ def _validate_audio_buffer(
         raise ValueError(INVALID_AUDIO_EMPTY_ERROR)
 
     if not isinstance(sample_rate, int):
-        raise ValueError(INVALID_SAMPLE_RATE_TYPE_ERROR)
+        raise ValueError(INVALID_SAMPLE_RATE_ERROR)
 
     if sample_rate <= 0:
-        raise ValueError(INVALID_SAMPLE_RATE_VALUE_ERROR)
+        raise ValueError(INVALID_SAMPLE_RATE_ERROR)
 
 
 def _validate_save_noise(
@@ -95,10 +95,10 @@ def _validate_generate_noise(
         raise ValueError(INVALID_DURATION_ERROR)
 
     if not isinstance(sample_rate, int):
-        raise ValueError(INVALID_SAMPLE_RATE_TYPE_ERROR)
+        raise ValueError(INVALID_SAMPLE_RATE_ERROR)
 
     if sample_rate <= 0:
-        raise ValueError(INVALID_SAMPLE_RATE_VALUE_ERROR)
+        raise ValueError(INVALID_SAMPLE_RATE_ERROR)
 
     if not isinstance(volume, (int, float)):
         raise ValueError(INVALID_VOLUME_TYPE_ERROR)
--- zenix/params.py
+++ zenix/params.py
@@ -23,8 +23,7 @@ INVALID_NOISE_TYPE_ERROR = "`noise_type` must be an instance of NoiseType."
 
 INVALID_DURATION_ERROR = "`duration` must be a number greater than 0."
 
-INVALID_SAMPLE_RATE_TYPE_ERROR = "`sample_rate` must be int."
-INVALID_SAMPLE_RATE_VALUE_ERROR = "`sample_rate` must be greater than 0."
+INVALID_SAMPLE_RATE_ERROR = "`sample_rate` must be a positive integer."
 
 INVALID_VOLUME_TYPE_ERROR = "`volume` must be int or float."
 INVALID_VOLUME_RANGE_ERROR = "`volume` must be between 0.0 and 1.0."
