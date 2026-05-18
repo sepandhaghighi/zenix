fbd6fdb146ef201c419b382b08f7e30a9d1b80d2
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-05-19 00:09:36 +0330

    feat : add errors.py

---
+++ zenix/errors.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""zenix errors.""""
+
+class ZenixError(Exception):
+    """Base exception for Zenix."""
+
+
+class ZenixValidationError(ZenixError, ValueError):
+    """Raised for invalid input parameters."""
