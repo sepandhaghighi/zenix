6bb4cb308526118b5b0152b268fe88aa3e84f998
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-06-04 17:47:40 +0330

    fix : move requirements-min to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 # -*- coding: utf-8 -*-
 """Setup module."""
-from typing import List
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
-
-def get_requires() -> List[str]:
-    """Read requirements.txt."""
-    requirements = open("requirements.txt", "r").read()
-    return list(filter(lambda x: x != "", requirements.split()))
 
 
 def read_description() -> str:
@@ -48,7 +41,7 @@ setup(
     keywords="noise white-noise pink-noise brown-noise audio sound dsp signal-processing cli productivity focus relaxation numpy",
     project_urls={
         'Source': 'https://github.com/sepandhaghighi/zenix'},
-    install_requires=get_requires(),
+    install_requires=['numpy>=1.20.0', 'nava>=0.4'],
     python_requires='>=3.7',
     classifiers=[
         'Development Status :: 4 - Beta',
