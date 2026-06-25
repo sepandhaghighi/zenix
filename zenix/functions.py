2277b04e24e5a8887a3ca0ffcd6d1f417c15d13d
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-06-25 16:45:33 +0330

    fix : update generate_noise function

--- zenix/functions.py
+++ zenix/functions.py
@@ -261,16 +261,7 @@ def generate_noise(
 
     samples = int(duration * sample_rate)
 
-    if noise_type == NoiseType.WHITE:
-        audio = _generate_white_noise(samples)
-    elif noise_type == NoiseType.PINK:
-        audio = _generate_pink_noise(samples)
-    elif noise_type == NoiseType.BROWN:
-        audio = _generate_brown_noise(samples)
-    elif noise_type == NoiseType.BLUE:
-        audio = _generate_blue_noise(samples)
-    else:
-        raise ZenixValidationError("Unsupported noise type")
+    audio = NOISE_GENERATORS[noise_type](samples)
 
     audio = _normalize(audio)
 
